72074bf63f68af735f1167c52d92f9a4c3c28064
rk0576 <70932901+rk0576@users.noreply.github.com>
2020-10-14 10:56:22 +0300

    Add files via upload

---
+++ pr1,1.py
@@ -0,0 +1,12 @@
+e1=int(input(" introduceti nr. elev curent e1=" ))
+p1=int(input(" introduceti punctaj"))
+e2=int(input(" introduceti nr.elev curent e2="))
+p2=int(input(" introduceti punctaj"))
+e3=int(input("introduceti nr.elev curent e3="))
+p3=int(input(" introduceti punctaj"))
+if (p1>p2) and (p2>p2):    
+    print(e1) 
+if (p2>p1) and (p2>p3):
+    print(e2)
+if (p3>p1) and (p3>p2):
+    print(e3)
