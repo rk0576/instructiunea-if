f794438824bf2ff5c847d4ac0258e0c4271c60e5
rk0576 <70932901+rk0576@users.noreply.github.com>
2020-10-18 14:10:58 +0300

    Add files via upload

---
+++ if12.py
@@ -0,0 +1,15 @@
+x=int(input("Numarul petalelor:"))
+if x>0:
+    print("Ma iubeste...")
+    if x%6==0:
+        print("...un pic")
+    elif x%6==5:
+        print("...de loc")
+    elif x%6==4:
+        print("...la nebunie")
+    elif x%6==3:
+        print("...cu pasiune")
+    elif x%6==2:
+        print("...mult")
+    else:
+        print("...un pic")
---
+++ if13.py
@@ -0,0 +1,9 @@
+x=int(input("introduceti numarul locului="))
+if 1<x<25:
+    print("culoarea alba")
+if 25<x<50:
+    print("culoarea rosie")
+if 50<x<75:
+    print("culoarea albastra")
+else:
+    print("culoarea neagra") 
