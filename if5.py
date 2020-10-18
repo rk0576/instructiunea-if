ac43c79b5048713b6a309364cd71a75c0fc6be31
rk0576 <70932901+rk0576@users.noreply.github.com>
2020-10-18 12:14:18 +0300

    Add files via upload

---
+++ if5.py
@@ -0,0 +1,10 @@
+ac=int(input("Introduceti anul curent:"))
+lc=int(input("Introduceti luna curenta:"))
+zc=int(input("Introduceti ziua curenta:"))
+an=int(input("Introduceti anul nasterii:"))
+ln=int(input("Introduceti luna nasterii:"))
+zn=int(input("Introduceti ziua nasterii:"))
+if (lc>ln):  25
+    print(ac-an+1)
+else:  
+    print (ac-an)
