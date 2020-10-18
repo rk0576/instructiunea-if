b72257d9a5b483a6cd522b42ebe0110b1abb101c
rk0576 <70932901+rk0576@users.noreply.github.com>
2020-10-18 10:48:23 +0300

    Add files via upload

---
+++ if4.py
@@ -0,0 +1,9 @@
+v1=int(input("varsta primei persoane:"))
+v2=int(input("varsta a doua persoane:"))
+v3=int(input("varsta a treiei persoane:"))
+if (v1>18) and (v1<60):
+    print(v1)
+if(v2>18) and (v2<60):  
+    print(v2)
+if (v3>18) and (v3<60):
+    print(v3)    
---
+++ if6.py
@@ -0,0 +1,12 @@
+a=int(input("Prima nota:"))
+b=int(input("A doua nota:"))
+c=int(input("A treia nota:"))    
+if c>=8:5
+    print(a,b,c)
+else:
+    if a>b:
+        print(a)
+    if a<b:
+        print(b)
+    if a==b:
+        print(a)
